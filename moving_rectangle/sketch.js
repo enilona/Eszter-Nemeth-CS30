e61d6701fc10b728d593cf61359fef5e36408a55
enilona <126793888+enilona@users.noreply.github.com>
2023-03-02 15:14:33 -0600

    rectangle
    make a rectangle

---
+++ moving_rectangle/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX, 100, 200, 50);
+}
