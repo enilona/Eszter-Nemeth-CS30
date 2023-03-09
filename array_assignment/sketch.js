4c552ccc53ec6efcad59d8cf9744547f3cc38d35
enilona <126793888+enilona@users.noreply.github.com>
2023-03-09 13:04:48 -0600

    I made weird lines

--- array_assignment/sketch.js
+++ array_assignment/sketch.js
@@ -8,8 +8,22 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(10)
+  drawLine(random(width));
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  
+}
+
+
+function drawLine(theLength){
+  stroke(random(250),80,200)
+  strokeWeight(2)
+  //fill(random(100,100),random(100,200),random(100,200))
+  //curve(100,100,250,250,300,300,400,400)
+  curve(0,random(height),random(width),random(height),random(width),random(height),width, random(height));
+  
+
 }
