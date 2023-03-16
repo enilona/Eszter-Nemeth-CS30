1a601115a0d67ca268ede9c5a0bd2d4f98e5ce97
enilona <126793888+enilona@users.noreply.github.com>
2023-03-16 15:19:32 -0600

    ahhhh

--- array_assignment/sketch.js
+++ array_assignment/sketch.js
@@ -13,39 +13,39 @@ let circles=[];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(3)
 
 }
 
 function draw() {
   background(220);
-
-  drawCircle();
+  drawCircles();
+  //justaCircle();
 }
 
-function drawCircle(){
+function drawCircles(){
 
-
-  for (let y = 0; y < height; y += 25) {
-    for (let x = 0; x < width; x += 25){
-      let x = noise(change1) * width;
-      let y = noise(change2) * height;
-      circle(x,y,25,25);
-      x = x + 1;
-      y = y + 50;
+  for (let y = 0; y < height; y += 45) {
+    for (let x = 0; x < width; x += 45){
+      let colorThing = noise(random(200));
+      let changeAmount1 = random(200,300);
+      let changeAmount2 = random(200,300);
+      let changeAmount3 = random(200,300);
+      fill(colorThing*changeAmount1,colorThing*500,colorThing*500)
+      circle(x,y,45,45)
     }
-    change1 += 0.001;
-    change2 += 0.001;
   }
-
-
 }
 
-
-function spawnCircle(theX,theY,theSize,thing){
-  let theCircles = {
-    x: theX,
-    y: theY,
-    s:theSize,
-  };
-  thing.push(theCircles);
+function justaCircle(){
+  fill("red");
+  let x = noise(change1) * width;
+  let y = noise(change2) * height;
+  circle(x,y,30);
+  circle(x+30,y,30)
+  x = x + 1;
+  y = y + 1;
+  change1 += 0.0001;
+  change2 += 0.0001;
+  noFill();
 }
