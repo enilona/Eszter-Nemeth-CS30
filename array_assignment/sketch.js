daa749884a6b6153e570e7685d986c80815d43bf
enilona <126793888+enilona@users.noreply.github.com>
2023-03-20 21:27:09 -0600

    Finished the array assignment!

--- array_assignment/sketch.js
+++ array_assignment/sketch.js
@@ -1,76 +1,95 @@
 // Arrays and Object Notation
 // Eszter Nemeth
-// Date
+// 2023 March 20
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// -Before we learned about it in class I a had already used arrays inside of arrays in my code.
 
+// explanation: This Generative art peice is designed so that a person can fiddle around with the different values and numbers to get completely different results. the first three grobal variables can be fiddled around with as well as the commented stuff inside the "drawShape" function. The code can also draw circes if the circle is uncommented in the "drawShape" function. It is also interesting to change the "noStroke" function inside the draw loop.
 
+//establishing global variables
+let NumberOfShapes = 7;
+let rectdiv = 9;
+let perimeterOfRect = 20;
 let Elements = [];
-let NumberOfCircles = 3;
+let rowscols;
+let  theSize = 20;
 let xStart = 30;
 let yStart = 30;
 let yDist = 60;
 let xDist = 60;
-let colorValue = noise(random(100)) * 200;
 
 
 function setup() {
-  createCanvas(500, 500);
-  createCircle(Elements);
-
+  createCanvas(windowWidth, windowHeight);
+  //controls how many rows and collums there are
+  rowscols = windowHeight/xDist;
+  createShape(Elements);
 }
 
 function draw() {
   background(220);
-  displayCircle();
+  //noStroke();
+  displayShape();
   noLoop();
-  
 }
 
-function displayCircle(){
+//goes through the lists and determines how to draw each shape
+function displayShape(){
   for (i = 0; i < Elements.length; i++){
-    circles = Elements[i]
-    for (j = 0; j < circles.length; j++){
-      C = circles[j];
-      drawCircle(C);
+    shapes = Elements[i]
+    for (j = 0; j < shapes.length; j++){
+      C = shapes[j];
+      drawShape(C);
     }
   }   
 }
  
-function drawCircle(C){
+//controls what the shapes look like
+function drawShape(C){
   fill(C.color);
-  rect(C.x - C.radius/2, C.y - C.radius/2, C.radius, C.radius)
-
+  if (C.type > 5){
+    rect(C.x - C.radius/2, C.y - C.radius/2, C.radius, C.radius)    
+  }
+  else {
+    //translate(C.x,C.y)
+    //rotate(y*0.03)
+    rect(C.x - C.radius/2, C.y - C.radius/2, C.radius/rectdiv, C.radius/rectdiv) 
+   // circle(C.x,C.y,C.radius)
+  }
 }
 
-function createCircle(Elements){
+//main part of the array
+function createShape(Elements){
+  //the array inside of the array
   let StackOfShapes = [];
-  R = 60;
-  // for (i = 0; i <= NumberOfElements; i++){
-    
-  for ( ri = 0; ri < 8; ri++){
-    for ( ci = 0; ci < 8; ci++){
+  
+  //creates a grid-like structure to draw the shapes
+  for ( ri = 0; ri < rowscols; ri++){
+    for ( ci = 0; ci < rowscols; ci++){
+      //starting point of the first shapes and number by which they increase throughout the grid
       y = yStart + ri * yDist;
       x = xStart + ci * xDist;
-      for (j = 0; j < NumberOfCircles; j++){
-    
-        Rc = R - j*20;
-      
-        theCircle = {
+      //controls how many shapes are inside the first shapes(stacks)
+      for (j = 0; j < NumberOfShapes; j++){
+        //how much each shape decreases
+        shapeSize = theSize - j*perimeterOfRect;
+        shapeType = random(10)
+        //creating object
+        theShape = {
+          type: shapeType,
           x: x, 
           y: y,
-          radius: Rc,
-    
-          color: color(colorValue, colorValue, colorValue),
+          radius: shapeSize,
+          color: color(noise(random(100)) * 300, noise(random(100)) * 300, noise(random(100)) * 300,10),
+          
         }  
-        StackOfShapes.push(theCircle);      
+        //adding objects to the group
+        StackOfShapes.push(theShape);      
       }
+      //adding the group to the total array
       Elements.push(StackOfShapes);
     }
   }
 }
   
-
-  
-
