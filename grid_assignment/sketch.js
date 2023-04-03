6488b5a3184bfca02b91806d4bfc54722bed068c
enilona <126793888+enilona@users.noreply.github.com>
2023-04-03 15:19:25 -0600

    worked on assignment

--- grid_assignment/sketch.js
+++ grid_assignment/sketch.js
@@ -5,12 +5,14 @@
 
 
 let grid;
+let numberList = [1,2,3,4,5,6,7,8,9];
 
 const ROWS = 35;
 const COLS = 35;
 let cellSize;
 let loadFile;
 let theColor;
+let someColor;
 
 function preload(){
   loadFile = "colors_list.txt";
@@ -31,18 +33,25 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(parseInt(theColor[7]));
   displayGrid();
 }
 
 function displayGrid(){
   for (let y = 0; y < ROWS; y++){
     for (let x = 0; x < COLS; x++){
-      let x = theColor[7];
-      fill(x);
+
+      fill(theColor[7]);
       rect (x*cellSize, y*cellSize, cellSize, cellSize);
+      textAlign(CENTER,CENTER);
+      textSize(20);
+      text(grid[y][x], x*cellSize + cellSize/2, y*cellSize + cellSize/2);
     }
   }
+}
+
+function numbersInGrid(){
+
 }
 
 function createRandomGrid(ROWS, COLS) {
